52d0199c514cf44bb14351d3467a595a4a2cdbd2
Adrian <adrianlineweaver@gmail.com>
2020-12-11 13:12:06 -0500

    added layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app'
+import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<Layout><Component {...pageProps} /></Layout>)
 }
 
 export default MyApp
